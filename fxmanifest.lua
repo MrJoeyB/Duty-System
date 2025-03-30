0afd7e42580adf5cba6184b648d8e91ce885a0e7
Next Dev Labs <144915098+NextLabsDevelopment@users.noreply.github.com>
2025-03-30 18:43:48 +0400

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,7 +5,10 @@ author 'Next Dev Labs'
 description 'Free Standalone Duty System'
 version '1.2'
 
-shared_script 'config.lua'
+shared_script {
+    '@ox_lib/init.lua',
+    'config.lua'
+}
 
 server_script 'server.lua'
 
