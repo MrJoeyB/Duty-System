a8d92b2994ad39e1ef50f67b57067f854523d347
Next Dev Labs <144915098+NextLabsDevelopment@users.noreply.github.com>
2025-03-30 10:43:05 +0400

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,15 +1,37 @@
 Config = {
-  WEBHOOK_URL = "",
-  ViewAce = "duty.view",
-  KickAce = "duty.kickoff",
-  DutyAce = "duty.clockin",
-  OffDutyACE = "duty.clockout"
-  AllowedDepartments = {
-      "LAPD",
-      "BCSO",
-      "ARMY",
-      "MARINES",
-      "DHS",
-      "CIA",
-  }
+    WEBHOOK_URL = "",
+    ViewAce = "duty.view",
+    KickAce = "duty.kickoff",
+    AllowedDepartments = {
+        {
+            name = "LAPD",
+            clockInAce = "duty.clockin.lapd",
+            clockOutAce = "duty.clockout.lapd"
+        },
+        {
+            name = "BCSO",
+            clockInAce = "duty.clockin.bcso",
+            clockOutAce = "duty.clockout.bcso"
+        },
+        {
+            name = "ARMY",
+            clockInAce = "duty.clockin.army",
+            clockOutAce = "duty.clockout.army"
+        },
+        {
+            name = "MARINES",
+            clockInAce = "duty.clockin.marines",
+            clockOutAce = "duty.clockout.marines"
+        },
+        {
+            name = "DHS",
+            clockInAce = "duty.clockin.dhs",
+            clockOutAce = "duty.clockout.dhs"
+        },
+        {
+            name = "CIA",
+            clockInAce = "duty.clockin.cia",
+            clockOutAce = "duty.clockout.cia"
+        }
+    }
 }
